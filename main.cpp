bcd776e2c78ea3c2e9f7b2b75d2115535ad076a5
Mario D Flores <43660822+SlimRunner@users.noreply.github.com>
2018-09-28 23:56:33 -0700

    Fresh project
    Initial state

---
+++ main.cpp
@@ -0,0 +1,16 @@
+//This project makes use of a structure to store and visualize student data
+//Mario D. Flores
+//September 28, 2018
+
+/*timetrack
+s1:start(9/28/18 23:52)end()span()brief()
+*/
+
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+
+}
